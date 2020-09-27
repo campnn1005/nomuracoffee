b321e623e53be77c9869b1745056da50cf41f482
野村尚未 <macmini@nomuranaohitsujinoMac-mini.local>
2020-09-27 18:20:01 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// document.addEventListener("DOMContentLoaded", () => {
+//     const js_accordion = document.getElementById('js-menu_switch');
+//     js_accordion.addEventListener('click', (e) => {
+//         js_accordion.parentNode.classList.toggle('is-hide');
+//     })
+// })
